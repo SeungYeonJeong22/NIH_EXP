3a085379b5d1c0c1fbee0112313b27f2afc210b2
seungyeonjeong22 <jq321@gmail.com>
2024-04-27 12:31:12 +0900

    M: calculate method softmax -> sigmoid

--- seungyeon/metric.py
+++ seungyeon/metric.py
@@ -4,9 +4,9 @@ import numpy as np
 def compute_metrics(predictions, targets, threshold=0.5):
     predictions = (predictions > threshold).astype(int)
     
-    print('Targets \t predictions')
-    for t, p in zip(targets, predictions):
-        print(t, '\t', p)
+    # print('Targets \t predictions')
+    # for t, p in zip(targets, predictions):
+    #     print(t, '\t', p)
     
     
     accuracy = accuracy_score(targets, predictions)
