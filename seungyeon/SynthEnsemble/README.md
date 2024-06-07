54511d7e31886f940df54a86af75ac68c940992f
Seungyeon Jeong <jq3210@gmail.com>
2024-06-07 14:44:35 +0900

    A: SyntheEnsemble Model

---
+++ seungyeon/SynthEnsemble/README.md
@@ -0,0 +1,17 @@
+# SynthEnsemble: A Fusion of CNN, Vision Transformer, and Hybrid Models for Multi-Label Chest X-Ray Classification
+[![PWC](https://img.shields.io/endpoint.svg?url=https://paperswithcode.com/badge/synthensemble-a-fusion-of-cnn-vision/multi-label-classification-on-chestx-ray14)](https://paperswithcode.com/sota/multi-label-classification-on-chestx-ray14?p=synthensemble-a-fusion-of-cnn-vision)
+
+Published in 2023 26th International Conference on Computer and Information Technology (ICCIT)
+
+# Publication Link
+https://ieeexplore.ieee.org/abstract/document/10441433
+
+# Training
+The training folder contains the notebook links for all 6 models: ConvNextV2, SwinV2, MaxVit, VOLO D2, CoAtNet, DenseNet121.
+# Testing
+The testing folder contains the notebooks for :
+1) running trained models on test dataset with average weighted ensemble
+2) finding optimal set of weights for weighted ensembling using differential evolution
+
+TODO:
+- Organize and upload notebooks used to train classical models as meta classifier
---
+++ seungyeon/SynthEnsemble/training/MaxViTV2/main.py
@@ -0,0 +1,109 @@
+import fastai
+from fastai.vision.all import *
+from tqdm import tqdm
+from glob import glob
+from sklearn.model_selection import train_test_split
+from fastai.vision.all import *
+
+
+SEED = 85
+def seed_everything(seed):
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+seed_everything(SEED)
+
+labels_train_val = pd.read_csv('../../../../../data/train_val_list.txt')
+labels_train_val.columns = ['Image_Index']
+labels_test = pd.read_csv('../../../../../data/test_list.txt')
+labels_test.columns = ['Image_Index']
+disease_labels = ['Atelectasis', 'Consolidation', 'Infiltration', 'Pneumothorax', 'Edema', 'Emphysema', 'Fibrosis', 'Effusion', 'Pneumonia', 'Pleural_Thickening',
+'Cardiomegaly', 'Nodule', 'Mass', 'Hernia']
+
+# NIH Dataset Labels CSV File 
+labels_df = pd.read_csv('../../../../../data/Data_Entry_2017.csv')
+labels_df.columns = ['Image_Index', 'Finding_Labels', 'Follow_Up_#', 'Patient_ID',
+                  'Patient_Age', 'Patient_Gender', 'View_Position',
+                  'Original_Image_Width', 'Original_Image_Height',
+                  'Original_Image_Pixel_Spacing_X',
+                  'Original_Image_Pixel_Spacing_Y', 'dfd']
+# One hot encoding
+for diseases in tqdm(disease_labels): 
+    labels_df[diseases] = labels_df['Finding_Labels'].map(lambda result: 1 if diseases in result else 0)
+
+labels_df['Finding_Labels'] = labels_df['Finding_Labels'].apply(lambda s: [l for l in str(s).split('|')])
+
+num_glob = glob.glob('../../../../../data/images_all/*.png')
+img_path = {os.path.basename(x): x for x in num_glob}
+
+labels_df['Paths'] = labels_df['Image_Index'].map(img_path.get)
+
+unique_patients = np.unique(labels_df['Patient_ID'])
+
+
+train_val_df_patients, test_df_patients = train_test_split(unique_patients, 
+                                   test_size = 0.2,
+                                   random_state = SEED,
+                                    shuffle= True
+                                   )
+
+train_val_df = labels_df[labels_df['Patient_ID'].isin(train_val_df_patients)]
+
+print('train_val size', train_val_df.shape[0])
+print('test size', labels_df.shape[0] - train_val_df.shape[0])
+
+item_transforms = [
+    Resize((224, 224)),
+]
+
+batch_transforms = [
+    Flip(),
+    Rotate(),
+    Normalize.from_stats(*imagenet_stats),
+]
+
+
+def get_x(row):
+    return row['Paths']
+
+def get_y(row):
+    labels = row[disease_labels].tolist()
+    return labels
+
+dblock = DataBlock(
+    blocks=(ImageBlock, MultiCategoryBlock(encoded=True,vocab=disease_labels)),
+                   splitter=RandomSplitter(valid_pct=0.125, seed=SEED),
+                   get_x=get_x,
+                   get_y=get_y,
+                   item_tfms=item_transforms,
+                   batch_tfms=batch_transforms
+                  )
+
+dls = dblock.dataloaders(train_val_df, bs=32)
+
+
+arch = 'maxxvitv2_rmlp_base_rw_224.sw_in12k_ft_in1k'
+
+cbs=[
+    SaveModelCallback(monitor='valid_loss', min_delta=0.0001, with_opt=True),
+    EarlyStoppingCallback(monitor='valid_loss', min_delta=0.0001, patience=4),
+    ShowGraphCallback()
+    ]
+
+learn = vision_learner(dls, arch, metrics=[accuracy_multi, F1ScoreMulti(), RocAucMulti()],cbs=cbs, wd=0.1)
+learn = learn.load('model')
+
+learn.model = torch.nn.DataParallel(learn.model)
+
+# import os
+# os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+# os.environ["TORCH_USE_CUDA_DSA"] = '1'
+
+# learn.fine_tune(freeze_epochs=3,epochs=10, base_lr=lrs.valley)
+learn.fit_one_cycle(10, 5e-6)
